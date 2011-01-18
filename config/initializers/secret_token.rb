436cffae8a7f0fda55c6aca81dc4ee55e1b8669f
urbanczykd <urbanczykd@gmail.com>
2011-01-18 11:34:35 +0100

    inicjalizacja

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vag::Application.config.secret_token = 'cf061e36c78f4de0acc80be8e4ce9b9cd095904eb0b493f2ffa431d7d8b76d7eee4d47d1156d49f6dd6b789f7669e4a67ee8cc51454324435d8e347ae1e06d36'
