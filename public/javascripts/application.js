68ee088769ac6e2def375001a1853e686b80eee2
urbanczykd <urbanczykd@gmail.com>
2011-01-20 22:31:17 +0100

    sc

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-$('#form_box').live('ajax:before', function(){
-alert('test');
+$(function() {
+$('#form_box form').bind('ajax:before', function(){
+$("#shoutbox_messages").html('laduje');
 });
-
+});
